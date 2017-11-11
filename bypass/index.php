b59f7ea4c11c653dc5ddd76bb7caf8a05fd013d5
Châu Thái Nhân <ctnhan044@gmail.com>
2017-11-11 11:34:37 +0700

    lasted

--- bypass/index.php
+++ bypass/index.php
@@ -1,1 +1,41 @@
-in
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="/bypass/style.css"/>
+      <title>Bypass Google Account 2017</title>
+      <meta name="author" content="Nhan Chau KP">
+      <link rel="canonical" href="http://tienich.xyz/">
+      <meta name="keywords" content="bypass google account, tải file bypass android, frp bypass account google, samsung bypass, android bypass">
+      <meta name="description" content="Bypass android account google, bypass hard reset, hướng dẫn bypass tài khoản google">
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="main">
+            <div class="header">
+               <h1>BYPASS GOOGLE ACCOUNT 2017</h1>
+            </div>
+            <div class="box">
+               <div class="catalog">
+               </div>
+               <ul>
+                  <p>
+                  <li><a href="/"><h2>Parent Directory</h2></a></li>
+                     <?php
+						$files = glob("*.{apk,exe}", GLOB_BRACE);
+                        echo implode('', array_map('sprintf', array_fill(0, count($files), '<li><a href="%s">%s</a></li>'), $files, $files));
+                        ?>
+                     </p>
+               </ul>
+               <ul class="ins">
+                  <li><a href="https://www.youtube.com/channel/UCs2pNuOsXT-4to26vRBCLbg" target="_blank"><b>Theo dõi tôi</b></a></li>
+               </ul>
+            </div>
+            <div class="footer">
+               <p>Copyright © 2017 <a href="/" target="_blank">Tienich.xYz</a> All Rights Reserved.</p>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
