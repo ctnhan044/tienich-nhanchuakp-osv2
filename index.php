36aa0ec633d8a4536eaf627c54c000456cef549a
Châu Thái Nhân <ctnhan044@gmail.com>
2018-06-03 12:14:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<html><center><Nhan Chau KP></center></html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Nhan Chau KP</title>
+    </head>
+    <body>
+      <center>&gt;Nhan Chau KP&lt;</center>
+    </body>
+</html>
